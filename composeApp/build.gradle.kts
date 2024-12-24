1a8b284e2e1b3d87d7ef49a734e05260a8ded8a7
Camarena, Javier <javier.camtri@gmail.com>
2024-12-24 15:11:58 +0100

    KmpMovies: Remove unused classes

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,7 +37,7 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
@@ -45,6 +45,9 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtime.compose)
         }
     }
+
+    // Prevent failures on `make`
+    task("testClasses")
 }
 
 android {
@@ -69,8 +72,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
--- composeApp/src/commonMain/kotlin/com/akole/kmp/movies/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.akole.kmp.movies
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
