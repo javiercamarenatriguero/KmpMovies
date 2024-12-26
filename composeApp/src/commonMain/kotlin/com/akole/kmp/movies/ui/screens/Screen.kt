5227baa9a86a24eacfe6d7c1d68138854b8cc2f8
Camarena, Javier <javier.camtri@gmail.com>
2024-12-26 09:48:55 +0100

    KmpMovies: Status bar

--- composeApp/src/commonMain/kotlin/com/akole/kmp/movies/ui/screens/Screen.kt
+++ composeApp/src/commonMain/kotlin/com/akole/kmp/movies/ui/screens/Screen.kt
@@ -1,8 +1,11 @@
 package com.akole.kmp.movies.ui.screens
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -11,7 +14,9 @@ fun Screen(
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ){
-    MaterialTheme {
+    val isDarkTheme = isSystemInDarkTheme()
+    val colorTheme = if (isDarkTheme) darkColorScheme() else lightColorScheme()
+    MaterialTheme(colorTheme) {
         Surface(
             modifier = modifier.fillMaxHeight(),
             content = content,
